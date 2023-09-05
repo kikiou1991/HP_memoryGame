96d098f839b62494327f557de128a20ab00752c9
Gabor Adorjani <gadorjani@windowslive.com>
2023-09-05 15:32:02 +0100

    logic to flip and check
    implemented logic to flip the cards and check if they have the same id, if they do change bcolor to green and leave them solved, if they dont show red bcolor and then after 1ms set the state back to default(so not showing the img)

--- hp-memory-game/src/components/card.js
+++ hp-memory-game/src/components/card.js
@@ -1,6 +1,8 @@
 function Card({item, id, handleClick}) {
+    const itemClass = item.stat ? " active " + item.stat : "";
+
     return (
-        <div className="card" onClick={() => handleClick(id)}>
+        <div className={"card" + itemClass} onClick={() => handleClick(id)}>
             <img src={item.img} alt={item.id}/>
         </div>
     )
