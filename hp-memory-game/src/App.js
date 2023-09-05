99f05d86425f4533adeb70a01d3a669c4c2e1fd5
Gabor Adorjani <gadorjani@windowslive.com>
2023-09-05 14:14:47 +0100

    second commit
    Created components for the cards and single cards as well as come styling to get started with the grid....created a simple 4x4 grid...also imported all images that later we will style and use in the game

--- hp-memory-game/src/App.js
+++ hp-memory-game/src/App.js
@@ -1,8 +1,9 @@
-
+import Cards from "./components/cards";
 function App() {
   return (
     <div className="App">
     <h1>Harry Potter Memory-Game</h1>
+    <Cards/>
     </div>
   );
 }
---
+++ hp-memory-game/src/components/cards.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import Card from "./card";
+
+function Cards() {
+const [items, setItems] = useState([
+    { id: 1, img: '../images/badger.png', stat: ""},
+    { id: 1, img: '../images/badger.png', stat: ""},
+    { id: 2, img: '../images/dumbledore.png', stat: ""},
+    { id: 2, img: '../images/dumbledore.png', stat: ""},
+    { id: 3, img: '../images/harry.jpg', stat: ""},
+    { id: 3, img: '../images/harry.jpg', stat: ""},
+    { id: 4, img: '../images/hermione.jpg', stat: ""},
+    { id: 4, img: '../images/hermione.jpg', stat: ""},
+    { id: 5, img: '../images/lion.jpg', stat: ""},
+    { id: 5, img: '../images/lion.jpg', stat: ""},
+    { id: 6, img: '../images/ravenclaw.jpg', stat: ""},
+    { id: 6, img: '../images/ravenclaw.jpg', stat: ""},
+    { id: 7, img: '../images/ron.jpg', stat: ""},
+    { id: 7, img: '../images/ron.jpg', stat: ""},
+    { id: 8, img: '../images/snake.jpg', stat: ""},
+    { id: 8, img: '../images/snake.jpg', stat: ""}
+   
+])
+ 
+    return(
+        <div className="container">
+            {items.map(item => (
+                <Card/>
+            ))}
+        </div>
+    )
+}
+
+export default Cards;
