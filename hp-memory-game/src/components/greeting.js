479b5ebe0b44ac56dd911ee22bb36dea3be7083c
Gabor Adorjani <gadorjani@windowslive.com>
2023-12-22 22:07:58 +0000

    updates

---
+++ hp-memory-game/src/components/greeting.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GreetingProfile = () => {
+    return (
+        <div className='hero'>
+            <img src="/images/wizard.png" alt="wizard"></img>
+        </div>
+    )
+}
+
+export default GreetingProfile
